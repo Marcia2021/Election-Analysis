0d6f75887967cfbd443c8668fe011609bb8198c2
Marcia2021 <smmseedling@hotmail.com>
2021-03-11 19:29:34 -0500

    Final version of the programs

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-voting_data=[]
-
-voting_data.append({"county":"Arapahoe","registered_voters":422829})
-voting_data.append({"county":"Denver","registered_voters":463353})
-voting_data.append({"countu":"Jefferson","registered_voters":432438})
-
